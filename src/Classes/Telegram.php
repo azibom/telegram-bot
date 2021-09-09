3586c2ea0e8dd31f50922f3824e007c6707b5c61
Azibom <mrsh13610@gmail.com>
2021-09-09 16:45:34 +0430

    init

---
+++ src/Classes/Telegram.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Classes;
+
+class Telegram {
+
+    CONST BASE_BOT_URL = "https://api.telegram.org/bot";
+    private $botToken;
+    private $botUrl;
+    private $content = null;
+
+    public function __construct($botToken)
+    {
+        $this->botToken = $botToken;
+        $this->botUrl = self::BASE_BOT_URL . $botToken . "/";
+    }
+
+    public function getUpdate()
+    {
+        $contents = file_get_contents("php://input");
+        $this->content = json_decode($contents, true);
+        
+        return $this->content;
+    }
+
+    public function getMessage()
+    {
+        $chatID = null;
+        $text   = null;
+
+        if ($this->content == null && isset($this->content["message"])) {
+            $message = $this->content["message"];
+            
+            $chatID = $message["chat"]["id"];
+            $text   = $message["text"];
+        }
+
+        return [
+            "chatID" => $chatID, 
+            "text" => $text, 
+        ];
+    }
+
+    public function sendMessage($chatID, $text)
+    {
+        $args = array(
+            "chatID" => $chatID,
+            "text" => $text,
+        );
+
+        file_get_contents($this->botUrl."sendMessage?".http_build_query($args));
+    }
+}
+
+
