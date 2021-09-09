e154954f2189f9db659a04046833ae1d3683d3bf
Azibom <mrsh13610@gmail.com>
2021-09-09 17:23:43 +0430

    add try catch

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require "./vendor/autoload.php";
 
 use TelegramBot\Classes\Telegram;
 
+
+$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+$txt = 'ffffff'."\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+
 try {
     $telegram = new Telegram("1997963802:AAHnNRQtNKdmdtquiHtPjeBzKkC2xCCAQzE");
     $telegram->getUpdate();
