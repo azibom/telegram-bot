fc6156adfd7fd930421c03d793fc694c72d36fcc
Azibom <mrsh13610@gmail.com>
2021-09-10 13:57:30 +0430

    update

--- index.php
+++ index.php
@@ -6,28 +6,31 @@ use TelegramBot\Classes\Telegram;
 
 try {
     $telegram = new Telegram("1997963802:AAHnNRQtNKdmdtquiHtPjeBzKkC2xCCAQzE");
+    $telegram->addMenu([
+        "news" => [
+            "HI1" => "SAMPLE",
+            "HI2" => "SAMPLE",
+            "HI3" => "SAMPLE",
+        ],
+        "aboutUs" => "hi I am a bot"
+    ]);
+
     $telegram->getUpdate();
-    $message = $telegram->getMessage();
-    $telegram->setMessage($message['chatID'], $message['text'])->addKeyboard(array(
-        array(
-            array("text" => "1", "callback_data" => "myCallbackData"),
-            array("text" => "2", "callback_data" => "myCallbackData")
-        ),
-        array(
-            array("text" => "3", "callback_data" => "myCallbackData"),
-            array("text" => "4", "callback_data" => "myCallbackData")
-        ),
-    ))->addInlineKeyboard(array(
-        array(
-            array("text" => "1", "callback_data" => "myCallbackData"),
-            array("text" => "2", "callback_data" => "myCallbackData")
-        ),
-        array(
-            array("text" => "3", "callback_data" => "myCallbackData"),
-            array("text" => "4", "callback_data" => "myCallbackData")
-        ),
-    ));
+    $telegram->setAnswer();
     $telegram->sendMessage();
+
+    // $message = $telegram->getMessage();
+    // $telegram->setMessage($message['chatID'], $message['text'])->addKeyboard(array(
+    //     array(
+    //         array("text" => "1"),
+    //         array("text" => "2")
+    //     ),
+    //     array(
+    //         array("text" => "3"),
+    //         array("text" => "4")
+    //     ),
+    // ));
+    // $telegram->sendMessage();
 
 } catch (Exception $e) {
     $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
@@ -35,83 +38,3 @@ try {
     fwrite($myfile, $txt);
     fclose($myfile);
 }
-
-
-// $t = array('ReplyKeyboardMarkup' => array('keyboard' => array(array("A", "B"))));
-
-// $replyMarkup = array(
-//     'inline_keyboard' => array(
-//         array("A", "B"),
-// 	array("C")
-//     )
-// );
-
-// $keyboard = array(
-//     "inline_keyboard" => array(
-//         array(
-//             array("text" => "1", "callback_data" => "myCallbackData"),
-//             array("text" => "2", "callback_data" => "myCallbackData")
-//         ),
-//         array(
-//             array("text" => "3", "callback_data" => "myCallbackData"),
-//             array("text" => "4", "callback_data" => "myCallbackData")
-//         ),
-//     )
-// );
-// $picwithhtml = <<<EOT
-// "<p>Like this<br>
-// <a href="https://res.cloudinary.com/practicaldev/image/fetch/s--KJhLmFDG--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/8f1y5bddj2d9aqngx4ol.png" class="article-body-image-wrapper"><img src="https://res.cloudinary.com/practicaldev/image/fetch/s--KJhLmFDG--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/8f1y5bddj2d9aqngx4ol.png" alt="Alt Text" loading="lazy"></a></p>
-
-// <h4>
-//   <a name="now-you-have-all-things-that-you-need-for-api-auth-" href="#now-you-have-all-things-that-you-need-for-api-auth-">
-//   </a>
-//   now you have all things that you need for api auth :)
-// </h4>
-
-// <h4>
-//   <a name="if-you-have-question-you-can-ask-it-here-" href="#if-you-have-question-you-can-ask-it-here-">
-//   </a>
-//   if you have question you can ask it here :)
-// </h4>
-
-// <h4>
-//   <a name="share-it-with-your-friends-if-you-like-it" href="#share-it-with-your-friends-if-you-like-it">
-//   </a>
-//   share it with your friends if you like it
-// </h4>
-// EOT;
-
-// $replyMarkupl = array(
-//     'inline_keyboard' => array("d","w")
-// );
-
-// if (isset($update["message"])){
-
-//     $chatID = $update["message"]["chat"]["id"];
-//     $text = $update["message"]["text"];
-
-//     $args = array(
-//         "chat_id" => $chatID,
-//         "text" => $picwithhtml,
-//         "parse_mode" => "HTML",
-//         "reply_markup" => json_encode($keyboard),
-//     );
-
-
-
-//     if ( $text == '/start' ) {
-//         // send welcome message
-//         file_get_contents($website."sendMessage?chat_id=".$chatID."&text=Welcome to my bot");
-//     }else{
-//         // send another message or do your logic (up to you)
-//         file_get_contents($website."sendMessage?".http_build_query($args));
-//     }
-
-//     $args = array(
-//         "chat_id" => $chatID,
-//         "photo" => 'https://image.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg',
-//         "reply_markup" => json_encode($keyboard),
-//     );
-    
-//     file_get_contents($website."sendPhoto?".http_build_query($args));
-// }
--- src/Classes/Telegram.php
+++ src/Classes/Telegram.php
@@ -5,15 +5,59 @@ namespace TelegramBot\Classes;
 class Telegram {
 
     CONST BASE_BOT_URL = "https://api.telegram.org/bot";
+    CONST NOT_FOUND = "دستور مورد نظر پیدا نشد";
+    CONST SELECT_ONE_ITEM = "لطفا یک دستور را انتخاب کنید";
     private $botToken;
     private $botUrl;
     private $content = null;
     private $message = null;
+    private $menu    = [];
 
     public function __construct($botToken)
     {
         $this->botToken = $botToken;
         $this->botUrl = self::BASE_BOT_URL . $botToken . "/";
+    }
+
+    public function addMenu($menu)
+    {
+        $this->menu = $menu;
+    }
+
+    public function setAnswer()
+    {
+        $inputMessage = $this->getMessage();
+        $searchResult = $this->searchInMenu($inputMessage['text'], $this->menu);
+        if ($searchResult == null) {
+            $this->setMessage($inputMessage['text'], self::NOT_FOUND);
+        } elseif (is_array($searchResult)) {
+            $array = [];
+            foreach ($searchResult as $key => $value) {
+                $array[] = array("text" => $key);
+            }
+            $this->setMessage($inputMessage['text'], self::SELECT_ONE_ITEM)->addKeyboard(array(
+                $array,
+            ));
+        } else {
+            $this->setMessage($inputMessage['text'], $searchResult);
+        }
+    }
+
+    public function searchInMenu($input, $menu)
+    {
+        foreach ($menu as $key => $value) {
+            if ($key == $input) {
+                return $value;
+            }
+            if (is_array($value)) {
+                $searchInMenuResult = $this->searchInMenu($input, $value);
+                if ($searchInMenuResult != null) {
+                    return $searchInMenuResult;
+                }
+            }
+        }
+
+        return null;
     }
 
     public function getUpdate()
