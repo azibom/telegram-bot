81dfa6f677a83de6a2350782eaab967f765f37d1
Azibom <mrsh13610@gmail.com>
2021-09-16 15:52:20 +0430

    update

--- index.php
+++ index.php
@@ -1,50 +1,35 @@
 <?php
 
 require "./vendor/autoload.php";
+require_once(__DIR__ . '/logger.php');
 
 use TelegramBot\Classes\Telegram;
 use TelegramBot\Classes\TelegramRepository;
 
-$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-fwrite($myfile, 'we are here 1' .get_current_user(). PHP_EOL);
-fclose($myfile);
+$logger = getLogger();
+$logger->info("hi");
 
-try {
-    $telegramRepository = new TelegramRepository();
-    $telegram = new Telegram("1997963802:AAHnNRQtNKdmdtquiHtPjeBzKkC2xCCAQzE", $telegramRepository);
-    $telegram->addMenu([
-        "درباره ی گربه ها" => [
-            "غذای گربه ها" => "غذای گربه ها خوب است",
-            "خواب گربه ها" => "خواب گربه باید مناسب باشد",
+$telegramRepository = new TelegramRepository();
+$telegram = new Telegram("1997963802:AAHnNRQtNKdmdtquiHtPjeBzKkC2xCCAQzE", $telegramRepository);
+$telegram->addMenu([
+    "درباره ی گربه ها" => [
+        "غذای گربه ها" => "غذای گربه ها خوب است",
+        "خواب گربه ها" => "خواب گربه باید مناسب باشد",
+    ],
+    "فروشگاه گربه" => [
+        "فندق" => "فندق گربه ی زیبایی است",
+        "قصه" => "قصه برای فروش نیست :|",
+    ],
+    "سوالات متداول" => [
+        "غذای گربه" => "غذای گربه باید مناسب باشد",
+        "وسایل مورد نیاز گریه" => [
+            "خاک گربه" => "خاک گربه برای کاشتن گربه استفاده نمیشود و برای دستشویی کردن گربه استفاده میشود",
+            "باکس گربه" => "باکس گربه بیشتر برای جایجایی گربه مورد استفاده قرار میگیرد",
         ],
-        "فروشگاه گربه" => [
-            "فندق" => "فندق گربه ی زیبایی است",
-            "قصه" => "قصه برای فروش نیست :|",
-        ],
-        "سوالات متداول" => [
-            "غذای گربه" => "غذای گربه باید مناسب باشد",
-            "وسایل مورد نیاز گریه" => [
-                "خاک گربه" => "خاک گربه برای کاشتن گربه استفاده نمیشود و برای دستشویی کردن گربه استفاده میشود",
-                "باکس گربه" => "باکس گربه بیشتر برای جایجایی گربه مورد استفاده قرار میگیرد",
-            ],
-        ],
-        "درباره ی ما" => "ما گوربه میفروشیم"
-    ]);
+    ],
+    "درباره ی ما" => "ما گوربه میفروشیم"
+]);
 
-    $res = $telegram->getUpdate();
-
-    $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-    fwrite($myfile, 'weeeeeeeeeeeeeeeeeeeeeeeeeeee' . PHP_EOL);
-    fwrite($myfile, json_encode($res) . PHP_EOL);
-    fwrite($myfile, 'weeeeeeeeeeeeeeeeeeeeeeeeeeee' . PHP_EOL);
-
-    fclose($myfile);
-
-    $telegram->setAnswer();
-    $telegram->sendMessage();
-} catch (Exception $e) {
-    $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-    $txt = 'Caught exception : '.$e->getMessage()."\n";
-    fwrite($myfile, $txt);
-    fclose($myfile);
-}
+$telegram->getUpdate();
+$telegram->setAnswer();
+$telegram->sendMessage();
